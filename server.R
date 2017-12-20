0aaf4e0f28f5aa4d1e3985752120a85a6353a741
Xiuyang Guan <xiuyangguan@wifi-roaming-128-4-93-40.host.udel.edu>
2017-12-20 15:11:02 -0500

    Update for the code

--- server.R
+++ server.R
@@ -28,9 +28,9 @@ shinyServer(function(input, output) {
     number_patients_targeted <- 
       patients_data_clean_10000 %>% 
         filter(prevalances == input$prevalance) %>%
-        nrow() 
+        nrow()
     valueBox(
-      number_patients_targeted, "Targeted patients", 
+      number_patients_targeted, "Selected patients", 
       color = "red"
     )
   })
@@ -40,42 +40,88 @@ shinyServer(function(input, output) {
     description <- analysis$descrition
     
     valueBox(
-      paste(round(description$age[1]/max(patients_data_clean_10000$patients_ID), 3) * 100, "%", sep = ""), 
-      "weigths",
+      round(description$weight[9]), "Average weigths",
       color = "orange"
     )
   })
   
-  # output$Average_age <- renderValueBox({
-  #   analysis <- personality_prevalances(input$prevalance, patients_data_clean_10000, input$top_n_relevant_prevalance)
-  #   description <- analysis$descrition
-  #   
-  #   valueBox(
-  #     round(description$age[9]), "Average age",
-  #     color = "yellow"
-  #   )
-  # })
+  output$Average_age <- renderValueBox({
+    analysis <- personality_prevalances(input$prevalance, patients_data_clean_10000, input$top_n_relevant_prevalance)
+    description <- analysis$descrition
+    
+    valueBox(
+      round(description$age[9]), "Average age",
+      color = "yellow"
+    )
+  })
   
   
   # The texts
   output$description <- renderUI({
     analysis <- personality_prevalances(input$prevalance, patients_data_clean_10000, input$top_n_relevant_prevalance)
     description <- analysis$descrition
-    
+
 
     HTML(paste(
-      "There are", 
-      paste("<b>", round(description$age[1]), "</b>"), 
-      "patients for the prevalance:", 
-      paste("<b>", input$prevalance, "</b><br/>"), 
+      "There are",
+      paste("<b>", round(description$age[1]), "</b>"),
+      "patients for the prevalance:",
+      paste("<b>", input$prevalance, "</b><br/>"),
       "The average age is",
       paste("<b>", round(description$age[9]), "years old", "</b><br/>"),
-      "The average weight is", 
+      "The average weight is",
       paste("<b>", round(description$weight[9]), "pounds", "</b>"))
     )
+
+  })
+  
+  output$introduction <- renderUI({
+    
+    HTML("<h2><center> The Drug Overdose Patients in PA</center></h2>
+         <p> This project aims to analyze the regional healthcare situation, based on the patient’s data in Philadelphia City. Using this project, the users can detect the following fields: </p>
+         <ul>
+            <li>The characteristics of the people who has the drug prevalance</li>
+            <li>The relationship between the drug overdose and other prevalance.</li>
+            <li>The overall situation of the Philadelphia City.</li>
+         </ul>
+         <p> The overall situation for the people and patients in Philadelphia City is: </p>")
+
+  })
+  
+  output$following <- renderUI({
+    
+    HTML("In the following part, there are two sections to analyze the reasons why the patients will have the drug overdose: 
+         <ul>
+            <li><b>The presonality reasons. </b>In this part, the weights, races, gender and different distribution will be explored.</li>
+            <li><b>The prevalances reasons. </b>In this part, the connected prevalances and the correlated ones will be detected and explored. </li>
+         </ul>
+         <p>In addition to the drug overdose, used the left side control and options bar, users can also choose the different focused prevalance and the number of related prevalance want to be focused.</p>")
     
   })
   
+  output$personality_introduction <- renderUI({
+    
+    HTML("<h2><center>The Presonality Analysis</center></h2>
+          <p> In this section, the analysis of the influence of the gender, age and race will be shown. We found that: 
+          <ul>
+            <li><b>There are more male patients than female ones </b></li>
+            <li><b>The black Americans, black Dominican and White Irish tend to be more drug overdosed</b></li>
+            <li><b>The Hispanic Central American has a severe sex inbalance, which means the female Hispanic Central American tends to have more drug overdoses</b></li>
+         </ul>")
+    
+  })
+  
+  output$prevalance_introduction <- renderUI({
+    
+    HTML("<h2><center>The Prevalance Analysis</center></h2>
+         <p> In this section, the analysis of the influence of the other prevalances will be shown. We found that: 
+         <ul>
+         <li><b>People who have prediabetes have the most possibility to have the drug overdose.</b></li>
+         <li><b>People who have hypertension are also very possible to have the drug overdose.</b></li>
+         <li><b>Applied association rules algorithm in the data, we can find that the ted molars can also be directed to the drug overdose. </b></li>
+         </ul>")
+    
+  })
   
   # The plots
   output$single_gender_plot <- renderPlot({
@@ -104,7 +150,7 @@ shinyServer(function(input, output) {
   })
   
   output$association_rules_net_plot_to_target <- renderPlot({
-    selected_prevalance_association <- input$prevalance_association
+    selected_prevalance_association <- input$prevalance
     rules <- apriori(data = prevalances_items, parameter=list(support=0.001, confidence=0.001), 
                      appearance = list(default="lhs", rhs = selected_prevalance_association), 
                      control = list(verbose = F))
@@ -114,7 +160,7 @@ shinyServer(function(input, output) {
   })
   
   output$association_rules_net_plot_from_target <- renderPlot({
-    selected_prevalance_association <- input$prevalance_association
+    selected_prevalance_association <- input$prevalance
     rules <- apriori(data = prevalances_items, parameter=list(support=0.001, confidence=0.001), 
                      appearance = list(default="rhs", lhs = selected_prevalance_association), 
                      control = list(verbose = F))
@@ -122,5 +168,64 @@ shinyServer(function(input, output) {
     plot(subrules2, method="graph", control=list(type="items") )
     
   })
+  
+  output$chord_diagram <- renderChorddiag({
+    
+    selected_prevalance_association <- input$prevalance
+    data_targeted_ID <- 
+      patients_data_clean_10000 %>%
+      filter(prevalances == selected_prevalance_association) %>%
+      select(patients_ID) %>%
+      unlist() %>%
+      as.numeric()
+    
+    data_targeted <- 
+      patients_data_clean_10000 %>%
+      filter(patients_ID %in% data_targeted_ID) %>%
+      group_by(prevalances) %>%
+      summarise(n = n()) %>%
+      filter(prevalances != selected_prevalance_association)
+    
+    names(data_targeted)[2] <- "drug_overdose" 
+    data_targeted_mat <- t(as.matrix(data_targeted[2]))
+    colnames(data_targeted_mat) <- as.character(data_targeted$prevalances)
+    
+    chorddiag(data_targeted_mat, type = "bipartite", showTicks = F, showGroupnames = F, 
+              groupnameFontsize = 14, groupnamePadding = 10, margin = 10)
+    
+  })
+  
+  output$treemap_personality <- renderD3tree3({
+    
+    selected_prevalance_association <- input$prevalance
+    
+    data_targeted <- 
+      patients_data_clean_10000 %>%
+      filter(prevalances == selected_prevalance_association) %>%
+      group_by(race) %>%
+      mutate(n_Male = ifelse(gender == "M", 1, 0), 
+             n_Female = ifelse(gender == "F", 1, 0)) %>%
+      summarise(race_sum = n(), sum_Male = sum(n_Male), sum_Female = sum(n_Female)) %>%
+      mutate(sum_Female/sum_Male)
+    
+    names(data_targeted)[5] <- "sex_ratio_Female_devide_Male"
+    
+    d3tree3(
+      treemap(data_targeted,
+            index=c("race"),
+            vSize="race_sum",
+            vColor="sex_ratio_Female_devide_Male",
+            type="value"), 
+      rootname = "Treemap for different race"
+    )
+    
+  })
+  
+  output$chord_diagram_title <- renderUI({
+    HTML("<h4><center><b> Chord Diagram for prevalances </b></center></h4>
+          <p><center><l> Please slip over the diagram to see that correlation </l></center></p>")
+    
+  })
+
   
 })
--- ui.R
+++ ui.R
@@ -2,36 +2,79 @@ library(shinydashboard)
 
 dashboardPage(
   
-  dashboardHeader(title = "Synthetics Analysis"),
+  dashboardHeader(title = "Synthea Project"),
   
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-      menuItem("Analysis", tabName = "analysis", icon = icon("th")),
-      menuItem("Exploration", tabName = "explore", icon = icon("cog"))
+      menuItem("Introduction", tabName = "introduction", icon = icon("dashboard")),
+      menuItem("Personality Reasons", tabName = "PersonalityReasons", icon = icon("cog")), 
+      menuItem("Prevalances Reasons", tabName = "PrevalancesReasons", icon = icon("th"))
+      
     ),
     selectInput("prevalance", "Please choose the prevalance:", prevalances_names, 
                 selectize = FALSE, selected = "drug_overdose"), 
-    sliderInput("top_n_relevant_prevalance", "top_n relevant prevalance", 1,20,5), 
-    sliderInput("total_patients_number", "total patients number", 1,56686,56686, step = 100), 
-    
-    selectInput("prevalance_association", "Please choose the prevalance for association rules:", 
-                prevalances_names, selectize = FALSE, selected = "drug_overdose")
+    sliderInput("top_n_relevant_prevalance", "top_n relevant prevalance", 1,20,5) 
+    # div("Author: Xiuyang Guan"), 
+    # div("Reference: https://www.shinyapps.io/admin/#/dashboard; 
+    #                 https://synthetichealth.github.io/synthea/
+    #                 https://factfinder.census.gov/
+    #                 https://www.vecteezy.com/free-vector/people")
+    # sliderInput("total_patients_number", "total patients number", 1,56686,56686, step = 100), 
+    # 
+    # selectInput("prevalance_association", "Please choose the prevalance for association rules:", 
+    #             prevalances_names, selectize = FALSE, selected = "drug_overdose")
   ),
   
   dashboardBody(
     tabItems(
-      tabItem(tabName = "dashboard",
+      tabItem(tabName = "introduction",
               
               fluidRow(
-                valueBoxOutput("Total_Patient_Box", width = 4), 
-                valueBoxOutput("Target_Patient_Box", width = 4), 
-                valueBoxOutput("Average_weight", width = 4)
+                box(htmlOutput("introduction"), width = 12)
+              ), 
+              
+              fluidRow(
+                valueBoxOutput("Total_Patient_Box", width = 6), 
+                valueBoxOutput("Target_Patient_Box", width = 6)
+                # valueBoxOutput("Average_weight", width = 3), 
                 # valueBoxOutput("Average_age", width = 3)
               ), 
               
               fluidRow(
-                box(htmlOutput("description"), width = 12)
+                box(htmlOutput("description"), width = 12), 
+                box(htmlOutput("following"), width = 12)
+              )
+              
+      ), 
+      
+      
+      
+      tabItem(tabName = "PersonalityReasons",
+              
+              fluidRow(
+                box(htmlOutput("personality_introduction"), width = 12)
+              ), 
+              
+              fluidRow(
+                box(plotOutput("single_gender_plot", height = "250px"), width = 4), 
+                box(plotOutput("single_race_plot", height = "250px"), width = 8), 
+                box(d3tree3Output("treemap_personality", width = "100%", height = "400px"), width = 12)
+              ), 
+              
+              fluidRow(
+                
+                box(plotOutput("age_density_plot", height = "150px"), width = 6), 
+                box(plotOutput("weight_density_plot", height = "150px"), width = 6)
+                
+              )
+              
+              
+      ), 
+      
+      tabItem(tabName = "PrevalancesReasons",
+              
+              fluidRow(
+                box(htmlOutput("prevalance_introduction"), width = 12)
               ), 
               
               fluidRow(
@@ -39,26 +82,10 @@ dashboardPage(
               ), 
               
               fluidRow(
-                box(plotOutput("single_gender_plot", height = "250px"), width = 4), 
-                box(plotOutput("single_race_plot", height = "250px"), width = 8)
-              ), 
-              
-              fluidRow(
-                
-                box(plotOutput("age_density_plot", height = "150px"), width = 6), 
-                box(plotOutput("weight_density_plot", height = "150px"), width = 6)
-              )
-              
-              
-            
-              
-      ), 
-      
-      tabItem(tabName = "analysis",
-              
-              fluidRow(
-                box(plotOutput("association_rules_net_plot_to_target"), width = 6), 
-                box(plotOutput("association_rules_net_plot_from_target"), width = 6)
+                box(htmlOutput("chord_diagram_title"),
+                    chorddiagOutput("chord_diagram"), 
+                    width = 6), 
+                box(plotOutput("association_rules_net_plot_to_target"), width = 6)
                 
               )
               
@@ -71,3 +98,4 @@ dashboardPage(
     
   )
 )
+
